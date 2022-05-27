0940c77a559be8690b422b4588e68415dde39862
Rohan Lamb <lambrohan@gmail.com>
2022-05-27 18:41:59 +0530

    added balance field  to web3 ctx

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { GameQualityOptions } from '../../Utils'
 import { Button } from '../Button/Button'
 import { Select } from '../Select/Select'
-import Web3Modal from 'web3modal'
 import useWeb3Ctx from '../../Context/Web3Context'
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
 	const [quality, setQuality] = useState(GameQualityOptions[0])
-	const { account, openModal } = useWeb3Ctx()
+	const { account, openModal, balance } = useWeb3Ctx()
+
 	return (
 		<header
 			id="header"
 			className="w-full bg-primary py-2 flex items-center justify-between md:pl-[9.875rem] md:pr-[8.563rem] px-4"
 		>
-			<img src="/doge.png" className="w-6 " />
+			<div className="flex items-center">
+				<img src="/doge.png" className="w-6 mr-4" />
+				{balance == undefined ? (
+					''
+				) : (
+					<h4 className="text-white text-xl font-semibold tracking-wider">
+						{(Number(balance) / Math.pow(10, 9)).toFixed(4)}{' '}
+						<span className="tracking-normal ml-2 font-normal uppercase text-sm">
+							Baby Doge Coin
+						</span>
+					</h4>
+				)}
+			</div>
+
 			<div className="flex items-center">
 				<Select
 					options={GameQualityOptions}
--- src/Context/Web3Context.tsx
+++ src/Context/Web3Context.tsx
@@ -3,12 +3,15 @@ import Web3 from 'web3'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import Web3Modal from 'web3modal'
 import CoinbaseWalletSDK from '@coinbase/wallet-sdk'
+import ABI from '../abi/babydoge.json'
 const providerOptions = {
 	/* See Provider Options Section */
 	walletconnect: {
 		package: WalletConnectProvider, // required
 		options: {
-			infuraId: '3a6c24b4ff074a3cb9ae9a74f3473db2', // required
+			rpc: {
+				56: 'https://speedy-nodes-nyc.moralis.io/362fc40c1ab324c15e79d4da/bsc/mainnet',
+			},
 		},
 	},
 	binancechainwallet: {
@@ -18,15 +21,14 @@ const providerOptions = {
 	coinbasewallet: {
 		package: CoinbaseWalletSDK, // Required
 		options: {
-			appName: 'My Awesome App', // Required
-			infuraId: '3a6c24b4ff074a3cb9ae9a74f3473db2', // Required
-			chainId: 1, // Optional. It defaults to 1 if not provided
+			appName: 'Baby Doge Coin', // Required
+			rpc: 'https://speedy-nodes-nyc.moralis.io/362fc40c1ab324c15e79d4da/bsc/mainnet', // Optional if `infuraId` is provided; otherwise it's required
+			chainId: 56, // Optional. It defaults to 1 if not provided
 		},
 	},
 }
 
 const web3Modal = new Web3Modal({
-	network: 'mainnet', // optional
 	providerOptions, // required
 	theme: {
 		background: '#fff',
@@ -39,22 +41,52 @@ const web3Modal = new Web3Modal({
 export type Web3ContextType = {
 	openModal: () => void
 	account: string | null
+	balance: string | number | undefined
 }
 export const Web3Context = createContext<Web3ContextType | null>(null)
 
 export const Web3Provider: React.FC<any> = (props) => {
 	const [account, setAccount] = useState<string | null>(null)
+	const [balance, setBalance] = useState<string | number | undefined>(undefined)
 
 	const openModal = async () => {
 		const provider = await web3Modal.connect()
+
+		try {
+			await window.ethereum.request({
+				method: 'wallet_switchEthereumChain',
+				params: [{ chainId: '0x38' }], // chainId must be in hexadecimal numbers
+			})
+		} catch (err: any) {
+			if (err.code === 4902)
+				window.ethereum.request({
+					method: 'wallet_addEthereumChain',
+					params: [
+						{
+							chainId: '0x38',
+							chainName: 'BSC mainnet',
+							nativeCurrency: {
+								name: 'Binance Coin',
+								symbol: 'BNB',
+								decimals: 18,
+							},
+							rpcUrls: ['https://bsc-dataseed.binance.org/'],
+							blockExplorerUrls: ['https://bscscan.com/'],
+						},
+					],
+				})
+		}
+
 		const web3 = new Web3(provider)
 		const [account] = await web3.eth.getAccounts()
+		const balance = await getBalance(provider)
+		setBalance(balance)
 		setAccount(account)
 		console.log('Account Address', account)
 	}
 
 	return (
-		<Web3Context.Provider value={{ account, openModal }}>
+		<Web3Context.Provider value={{ account, openModal, balance }}>
 			{props.children}
 		</Web3Context.Provider>
 	)
@@ -62,4 +94,24 @@ export const Web3Provider: React.FC<any> = (props) => {
 
 export default function useWeb3Ctx(): Web3ContextType {
 	return useContext(Web3Context) as Web3ContextType
+}
+
+export const getBalance = (provider: any): Promise<string> => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const web3 = new Web3(provider)
+			const [account] = await web3.eth.getAccounts()
+			const contract = new web3.eth.Contract(
+				ABI as any,
+				'0xc748673057861a797275CD8A068AbB95A902e8de'
+			)
+			const balance = (await contract.methods
+				.balanceOf(account)
+				.call()) as string
+
+			resolve(balance)
+		} catch (error) {
+			reject(error)
+		}
+	})
 }
