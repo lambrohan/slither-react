485897c517488bb75471b8c57d3c5aeabd3f6629
Kartik <kr.pjpt@gmail.com>
2022-06-08 23:32:47 +0530

    updates

--- src/Layouts/Lobby/Lobby.tsx
+++ src/Layouts/Lobby/Lobby.tsx
@@ -8,7 +8,7 @@ interface LobbyProps {}
 export const Lobby: React.FC<LobbyProps> = ({}) => {
 	return (
 		<div
-			className="w-full min-h-screen overflow-auto lg:max-h-screen lg:overflow-hidden bg-no-repeat bg-cover bg-center"
+			className="w-full min-h-screen overflow-auto lg:max-h-screen bg-no-repeat bg-cover bg-center"
 			id="lobby"
 		>
 			<Header />
