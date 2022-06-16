7a639e320cb3dc3e768b1716a081ce0db1167ece
Rohan Lamb <lambrohan@gmail.com>
2022-06-16 22:21:33 +0530

    ts errors refractor

--- src/Pages/Congratulation.tsx
+++ src/Pages/Congratulation.tsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import {Congratulation} from "../Components/ModalContent/Congratulation"
-import SlitherImage from "../assets/images/SlitherImage.svg"
+import { Congratulation } from '../Components/ModalContent/Congratulation'
+import SlitherImage from '../assets/images/SlitherImage.svg'
 
 interface CongratulationPageProps {}
 
 export const CongratulationPage: React.FC<CongratulationPageProps> = ({}) => {
-
 	return (
-	<>
-  <div className=" lg:h-full my-6 mx-2 flex items-center flex-col justify-center" >
-    <img className="mx-auto mb-1" src={SlitherImage} alt="SlitherImage"/>
-    <Congratulation />
-    </div>
-  </>
+		<>
+			<div className=" lg:h-full my-6 mx-2 flex items-center flex-col justify-center">
+				<img className="mx-auto mb-1" src={SlitherImage} alt="SlitherImage" />
+				<Congratulation tokens="0" rank="0" />
+			</div>
+		</>
 	)
 }
