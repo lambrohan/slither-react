37e82d28f8c0a402a7bef7413cd7b44cda9f3be0
Rohan Lamb <lambrohan@gmail.com>
2022-06-16 22:23:45 +0530

    ts error fix

--- src/Pages/Congratulation.tsx
+++ src/Pages/Congratulation.tsx
@@ -9,7 +9,7 @@ export const CongratulationPage: React.FC<CongratulationPageProps> = ({}) => {
 		<>
 			<div className=" lg:h-full my-6 mx-2 flex items-center flex-col justify-center">
 				<img className="mx-auto mb-1" src={SlitherImage} alt="SlitherImage" />
-				<Congratulation tokens="0" rank="0" />
+				<Congratulation tokens="0" rank="0" onNext={() => {}} />
 			</div>
 		</>
 	)
